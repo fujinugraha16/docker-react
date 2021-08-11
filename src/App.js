f3084d6927df1d05d8430a755e03debcd9e7cef0
fujinugraha16 <fujinugraha16@gmail.com>
2021-08-11 20:06:37 +0700

    automated deployments

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{"Hi. fuji :)"}</p>
+        <p>{"Hi. fuji :), welcome to aws"}</p>
       </header>
     </div>
   );
